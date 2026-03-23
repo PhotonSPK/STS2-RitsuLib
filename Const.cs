9a203d9e5468bccd146260031611d2f0ebba9142
OLC <utsumabo@ritsukage.com>
2026-03-23 23:33:56 +0800

    feat: Update version to 0.0.10 in Const, project file, and mod manifest

--- Const.cs
+++ Const.cs
@@ -4,7 +4,7 @@ namespace STS2RitsuLib
     {
         public const string Name = "RitsuLib";
         public const string ModId = "com.ritsukage.sts2-RitsuLib";
-        public const string Version = "0.0.9";
+        public const string Version = "0.0.10";
         public const string SettingsKey = "settings";
         public const string SettingsFileName = "settings.json";
     }
